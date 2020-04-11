13453c7f1fcb4055ff404b0f7d5b1c00241df2f2
Ronan Girault <girault.ronan@gmail.com>
2020-04-11 11:49:32 +0200

    init commit

---
+++ src/main/java/guru/springframework/udemy/SimpleWebAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebAppApplication.class, args);
+	}
+
+}
